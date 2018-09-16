0a5e546114f3334e79ef1e8f6fd7cbee80c1ad41
Forrest Collman <forrest.collman@gmail.com>
2018-09-16 16:06:36 -0700

    fix static config

--- annotationinfoservice/__init__.py
+++ annotationinfoservice/__init__.py
@@ -14,7 +14,8 @@ def create_app(test_config=None):
     # Define the Flask Object
     app = Flask(__name__,
                 instance_path=get_instance_folder_path(),
-                instance_relative_config=True)
+                instance_relative_config=True,
+                static_folder="../static")
     # load configuration (from test_config if passed)
     if test_config is None:
         app = configure_app(app)
