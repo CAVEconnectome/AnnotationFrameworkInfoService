3b6e2facb434b91ea2aa534cde41c81b20e8c7eb
Forrest Collman <forrest.collman@gmail.com>
2018-09-16 15:00:33 -0700

    improved web views

--- annotationinfoservice/datasets/controllers.py
+++ annotationinfoservice/datasets/controllers.py
@@ -16,17 +16,23 @@ def index():
 @mod_datasets.route("/dataset/<datasetname>")
 def dataset_view(datasetname):
     dataset = DataSet.query.filter(DataSet.name == datasetname).first_or_404()
-    neuroglancer
-    return render_template('dataset.html', dataset=dataset)
+    state = neuroglancer.ViewerState()
+    state.layers['img'] = neuroglancer.ImageLayer(source=dataset.image_source)
+    state.layers['seg'] = neuroglancer.SegmentationLayer(source=dataset.flat_segmentation_source)
+    state.layers['ann'] = neuroglancer.AnnotationLayer()
+    state.layout = "xy-3d"
+    ng_url = neuroglancer.to_url(state,
+                                 prefix=current_app.config['NEUROGLANCER_URL'])
+    return render_template('dataset.html', dataset=dataset, ng_url=ng_url)
 
 
-@mod_datasets.route("/api/")
+@mod_datasets.route("/api/datasets")
 def get_datasets():
     datasets = DataSet.query.all()
-    return jsonify([d.name for d in datasets], many=True)
+    return jsonify([d.name for d in datasets])
 
 
-@mod_datasets.route("/api/<dataset>", methods=['GET'])
+@mod_datasets.route("/api/dataset/<dataset>", methods=['GET'])
 def get_dataset(dataset):
     dataset = DataSet.query.filter_by(name=dataset).first_or_404()
     schema = DataSetSchema()
