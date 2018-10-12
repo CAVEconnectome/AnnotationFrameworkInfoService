ac2fe0b943f1eb7dc09e3c70a80f4a89afd278a7
Forrest Collman <forrest.collman@gmail.com>
2018-10-12 10:34:02 -0700

    adding versions

--- annotationinfoservice/datasets/controllers.py
+++ annotationinfoservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint('datasets', __name__, url_prefix='/datasets')
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 @mod_datasets.route("/")
 def index():
     datasets = DataSet.query.all()
