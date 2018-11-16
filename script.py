257420d9ea2d5bda91e39206eb364c939cf80ace
Forrest Collman <forrest.collman@gmail.com>
2018-11-16 14:33:45 -0800

    adding script changes

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from annotationinfoservice.database import Base
 from annotationinfoservice.datasets.models import DataSet
-engine = create_engine("postgres://postgres:postgres@localhost:5432/postgres")
+engine = create_engine("postgres://postgres:synapsedb@localhost:5432/infoservice")
 session = sessionmaker(bind=engine)
 db_session = scoped_session(session)
 
@@ -12,8 +12,30 @@ Base.query = db_session.query_property()
 
 # Create Tables
 Base.metadata.create_all(bind=engine)
+dataset = DataSet(name="pinky100",
+                  flat_segmentation_source="gs://neuroglancer/pinky100_v0/seg/lost_no-random/bbox1_0",
+                  annotation_engine_endpoint="https://www.dynamicannotationengine.com/",
+                  image_source="gs://neuroglancer/pinky100_v0/son_of_alignment_v15_rechunked",
+                  pychunkgraph_segmentation_source="gs://neuroglancer/nkem/pinky100_v0/ws/lost_no-random/bbox1_0",
+                  pychunkgraph_endpoint="http://35.237.202.194")
 
-dataset = DataSet(name="test", flat_segmentation_url="http://test_url")
 db_session.add(dataset)
+
+dataset = DataSet(name="basil",
+                  flat_segmentation_source="gs://neuroglancer/basil_v0/basil_full/seg-aug",
+                  annotation_engine_endpoint="https://www.dynamicannotationengine.com/",
+                  image_source="gs://neuroglancer/basil_v0/son_of_alignment/v3.04_cracks_only_normalized_rechunked",
+                  pychunkgraph_segmentation_source="",
+                  pychunkgraph_endpoint="http://35.237.202.194")
+db_session.add(dataset)
+
+dataset = DataSet(name="pinky100dev",
+                  flat_segmentation_source="https://storage.googleapis.com/neuroglancer/pinky100_v0/seg/lost_no-random/bbox1_0",
+                  annotation_engine_endpoint="https://www.dynamicannotationengine.com/",
+                  image_source="https://storage.googleapis.com/neuroglancer/pinky100_v0/son_of_alignment_v15_rechunked",
+                  pychunkgraph_segmentation_source="https://storage.googleapis.com/neuroglancer/nkem/pinky100_v0/ws/lost_no-random/bbox1_0",
+                  pychunkgraph_endpoint="http://35.237.202.194")
+db_session.add(dataset)
+
 db_session.commit()
 print(DataSet.query.all())
