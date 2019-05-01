ed1cfc043fc09e22a31b36e14d03292e747e6376
Forrest Collman <forrest.collman@gmail.com>
2019-05-01 08:08:36 -0700

    fixing prod server script

--- run_prodserver.sh
+++ run_prodserver.sh
@@ -1,5 +1,5 @@
 #! /bin/bash 
 export FLASK_APP=run.py
 export FLASK_ENV=development
-export ANNOTATIONINFOSERVICE_SETTINGS=$PWD/annotationinfo/instance/prod_config.py
+export ANNOTATIONINFOSERVICE_SETTINGS=$PWD/annotationinfoservice/instance/prod_config.py
 flask run
