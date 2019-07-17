d48d93736035341c43174609df4955a24255ed26
Forrest Collman <forrest.collman@gmail.com>
2019-07-17 09:49:49 -0700

    Bump version: 0.2.1 → 0.3.0

--- annotationinfoservice/datasets/controllers.py
+++ annotationinfoservice/datasets/controllers.py
@@ -6,7 +6,7 @@ import neuroglancer
 
 mod_datasets = Blueprint('datasets', __name__, url_prefix='/datasets')
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 @mod_datasets.route("/")
 def index():
     datasets = DataSet.query.all()
