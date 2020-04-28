f74242f83d51f90bc8d99404fc77147585df0559
dlbrittain <dlbrittain@gmail.com>
2020-04-27 19:31:42 -0700

    Fixed flask_marshmallow api imports

--- annotationinfoservice/datasets/schemas.py
+++ annotationinfoservice/datasets/schemas.py
@@ -3,11 +3,11 @@ from flask_marshmallow import Marshmallow
 
 ma = Marshmallow()
 
-class DataSetSchema(ma.ModelSchema):
+class DataSetSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = models.DataSet
 
-class DataSetSchemaV2(ma.ModelSchema):
+class DataSetSchemaV2(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = models.DataSetV2
 
