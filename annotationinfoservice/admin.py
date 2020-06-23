cc6534e0fb0f6506705bf62bf3700c9d264a19ba
Forrest Collman <forrest.collman@gmail.com>
2020-06-23 15:24:01 -0700

    fixing admin

--- annotationinfoservice/admin.py
+++ annotationinfoservice/admin.py
@@ -2,25 +2,17 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from annotationinfoservice.datasets.models import DataStack,\
      TableMapping, PermissionGroup, AlignedVolume
-from middle_auth_client import auth_requires_admin
-from flask import redirect, url_for
-
-@auth_requires_admin
-def test_super_admin():
-     return "is_super_admin"
+from middle_auth_client import auth_requires_admin, auth_required
+from flask import redirect, url_for, request, g
 
 class SuperAdminView(ModelView):
-     
+     @auth_required
      def is_accessible(self):
-          resp = test_super_admin()
-          if resp=="is_super_admin":
-               return True    
-          else:
-               return False
+        return g.auth_user['admin']
                
      def inaccessible_callback(self, name, **kwargs):
         # redirect to login page if user doesn't have access
-        return redirect(url_for('views.index', next=request.url))
+        return redirect(url_for('admin.index'))
 
 def setup_admin(app, db):
     admin = Admin(app, name="infoservice admin", url='/info/admin')
