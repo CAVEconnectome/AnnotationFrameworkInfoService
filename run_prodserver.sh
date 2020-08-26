851f9e67cc20734378f9d236a4a441d9ca17147f
Forrest Collman <forrest.collman@gmail.com>
2020-08-25 17:46:56 -0700

    changing running scripts

--- run_prodserver.sh
+++ run_prodserver.sh
@@ -2,4 +2,7 @@
 export FLASK_APP=run.py
 export FLASK_ENV=development
 export ANNOTATIONINFOSERVICE_SETTINGS=$PWD/annotationinfoservice/instance/prod_config.py
-flask run
+export AUTH_URI=global.daf-apis.com/auth
+export STICKY_AUTH_URL=global.daf-apis.com/sticky_auth
+python run.py
+
