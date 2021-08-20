1620f6f6794fd25e685cdb2206c62438a84563b8
Forrest Collman <forrest.collman@gmail.com>
2021-08-20 08:35:21 -0700

    Bump version: 3.4.4 → 3.4.5

--- annotationinfoservice/datasets/views.py
+++ annotationinfoservice/datasets/views.py
@@ -17,7 +17,7 @@ from middle_auth_client import (
 import flask
 import os
 
-__version__ = "3.4.4"
+__version__ = "3.4.5"
 
 views_bp = Blueprint("datastacks", __name__, url_prefix="/datastacks")
 
