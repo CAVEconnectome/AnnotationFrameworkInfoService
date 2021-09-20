220a30777b025bd2893c708e3ea09a35f7d76c24
Forrest Collman <forrest.collman@gmail.com>
2021-09-20 16:04:29 -0700

    Bump version: 3.6.0 → 3.7.0

--- annotationinfoservice/datasets/api.py
+++ annotationinfoservice/datasets/api.py
@@ -16,7 +16,7 @@ from middle_auth_client import (
     user_has_permission,
 )
 
-__version__ = "3.6.0"
+__version__ = "3.7.0"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
