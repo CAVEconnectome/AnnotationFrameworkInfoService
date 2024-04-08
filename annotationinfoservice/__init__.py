fd8ca38ea2515f00510127da16941b7b2e194b7d
Forrest Collman <forrest.collman@gmail.com>
2024-04-08 11:32:46 -0700

    Bump version: 4.0.2 → 4.0.3

--- annotationinfoservice/__init__.py
+++ annotationinfoservice/__init__.py
@@ -14,7 +14,7 @@ from flask_migrate import Migrate
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "4.0.2"
+__version__ = "4.0.3"
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
--- annotationinfoservice/datasets/api.py
+++ annotationinfoservice/datasets/api.py
@@ -15,7 +15,7 @@ from middle_auth_client import (
     user_has_permission,
 )
 
-__version__ = "4.0.2"
+__version__ = "4.0.3"
 
 
 authorizations = {
