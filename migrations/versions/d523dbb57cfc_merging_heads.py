d4b25d09a05d6df3171f7ecff5399b5a15f9b89a
Forrest Collman <forrest.collman@gmail.com>
2024-04-08 11:30:22 -0700

    merging heads of migration

---
+++ migrations/versions/d523dbb57cfc_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: d523dbb57cfc
+Revises: 6c4b42b01045, 99ebe4492cee
+Create Date: 2024-04-08 11:30:10.536470
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd523dbb57cfc'
+down_revision = ('6c4b42b01045', '99ebe4492cee')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
