c5530fbc60843277748bcf33d9157cc73730c701
Forrest Collman <forrest.collman@gmail.com>
2024-07-15 08:50:01 -0600

    Bump version: 4.0.3 → 4.1.0

--- annotationinfoservice/__init__.py
+++ annotationinfoservice/__init__.py
@@ -14,7 +14,7 @@ from flask_migrate import Migrate
 from werkzeug.middleware.proxy_fix import ProxyFix
 from middle_auth_client import auth_required
 
-__version__ = "4.0.3"
+__version__ = "4.1.0"
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
